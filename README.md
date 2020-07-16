3f553fee73611f8827c0f4e1a387967ab78db826
Will Kendall <wkcodes2020@gmail.com>
2020-07-16 15:25:17 -0700

    Added final changes before submission

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WK_Password_Generator
+
+A simple password generator application.
--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const symbols = ["!", "#", "$", "%", "*"];
+
+  //Get Dom elements
+  let length = document.getElementById("passLength").value;
+  let lower = document.getElementById("passLower");
+  const upper = document.getElementById("passUpper");
+  const number = document.getElementById("includeNumbers");
+  const symbol = document.getElementById("passSymbols");
+
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,41 +25,37 @@ function writePassword() {
 
 // Generate the password
 function generatePassword() {
-  //Get Dom elements
-  const length = document.getElementById("passLength").value;
-  const number = document.getElementById("includeNumbers").value;
-  console.log(number);
-
   //Add characters to the password
   let password = "";
-  while(password.length < length){
-  password += getRandomLowercase();
-  password += getRandomUppercase();
-  password += getRandomNumer();
-  password += getRandomSymbol();
+  var availableChars = [];
+
+  for (i = 0; password.length < length; i++) {
+
+    if (lower.checked == true) {
+      password += getRandomCharacter(lowerChars);
+      availableChars.concat(lowerChars);
+    }
+    if (upper.checked == true) {
+      password += getRandomCharacter(upperChars);
+      availableChars.concat(upperChars);
+    }
+    if (number.checked == true) {
+      password += getRandomCharacter(numbers);
+      availableChars.concat(numbers);
+    }
+    if (symbol.checked == true) {
+      password += getRandomCharacter(symbols);
+      availableChars.concat(symbols);
+    }
+    
   }
-  return password;
-}
+    return password;
+  }
 
-// random uppercase letter function
-function getRandomUppercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-// random lowercase letter function 
-function getRandomLowercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-// random number function
-function getRandomNumer() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-//random symbol function
-function getRandomSymbol() {
-  const symbols = "!@#$%^&*";
-  return symbols[Math.floor(Math.random() * 8)];
+//Get random char from array
+function getRandomCharacter(charArray) {
+  var random = charArray[Math.floor(Math.random() * charArray.length)];
+  return random;
 }
 
 // Add event listener to generate button
